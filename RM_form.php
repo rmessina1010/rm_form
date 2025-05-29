86cf7b6510df8ce4c2228341b74481e029ec1e4e
rmessina1010 <rmessina10@gmail.com>
2025-05-28 23:07:18 -0500

    elaborates multi page

--- RM_form.php
+++ RM_form.php
@@ -1,3 +1,6 @@
+<?php 
+	ini_set('display_errors', 1); 
+?> 
 <style>
  	form div, form p, form h2, form h3{
 		padding: .5em 1em;
@@ -32,6 +35,9 @@
 	class RM_form {
 		protected $attr_keys	= ["char"=>"accept-charset", "act"=>"action", "auto"=>"autocomplete", "class"=>"class","enc"=>"enctype", "id"=>"id", 										"mtd"=>"method", "name"=>"name", "noval"=> "novalidate", "tgt"=>"target", "style"=>"style"];
 		protected $attrs		= array();
+		protected $form_name	= '';
+		protected $css			= '';
+		protected $is_css_url 	= false;
 		protected $pg_ct 		= 0;
 		protected $on_pg 		= 0;
 		protected $is_valid 	= false;
@@ -41,6 +47,7 @@
 		protected $form_data 	= array();
 		protected $is_sub		= false;
 		protected $is_nav		= false;
+		protected $is_pg_valid	= false;
 		protected $navs			= array();
 		protected $is_processed	= false;
 		protected $sub 			= '';
@@ -51,29 +58,38 @@
 		protected $sub_html		= '';
 		
 		
-		function RM_form($sub, $args = array()){
+		function __construct($sub, $args = array()){
+			if (!headers_sent() && session_status() === PHP_SESSION_NONE) { session_start(); }
+		
+
 			foreach($args  as $possible_attr_key => $att_val){
 				if (isset($this->attr_keys[$possible_attr_key])){ 
 					$this->attrs[$this->attr_keys[$possible_attr_key]] = $att_val; 
 				}
-				elseif (substr($possible_attr, 0, 2) === "on" ||  substr($possible_attr, 0, 5) === "aria-" || substr($possible_attr, 0, 5) === "data-"){ 
-					$this->attrs[$attr_name] = $args[$attr_key]; 
+				elseif (substr($possible_attr_key, 0, 2) === "on" ||  substr($possible_attr_key, 0, 5) === "aria-" || substr($possible_attr_key, 0, 5) === "data-"){ 
+					$this->attrs[$possible_attr_key] =  $att_val; 
 				}
 			}
-			$this->method = isset($args['mtd']) ? strtoupper($args['mtd']) : 'GET';
-			$this->sub =$sub;
-			if (isset($arg['navs']) && is_array($arg['navs'])){ $this->navs = $arg['navs'];}
+			$this->method = isset($args['mtd']) && $args['mtd'] ? strtoupper($args['mtd']) : 'GET';
+ 			$this->sub =$sub;
+			if (isset($args['navs']) && is_array($args['navs'])){ $this->navs = $args['navs']; }
 			$this->pg_ct =count($this->pages);
-			$this->checksub('xx');
- 		}
+			$this->form_name = isset($this->attrs['name']) ?  $this->attrs['name'] : 'dflt';
+  		}
+  		
+  		protected function style(){
+	  		return  $this->css;
+  		}
 
 		protected function setErr($field, $message, $delim=' '){
 			$this->errs[$field] =  isset($this->errs[$field]) ?  $this->errs[$field].$delim.$message."\n": $message;
-			if (!$this->kp_inv_data){ $this->methodVars[$field] = '';}
+			if (!$this->kp_inv_data){ $this->set_methodVar($field, '');}
 		}
 
 		function form_body_preprocess($html){
-			return $this->{$html}();
+			if (method_exists($this, $html)) { return $this->{$html}();}
+			if (isset($this->{$html}) && is_string($this->{$html})) {  return $this->{$html}; }
+			return  $html;
 		}
 		
 		function form_body(){
@@ -110,17 +126,47 @@
 		
 		function checksub($with = null ){
  			$this->is_sub =array_key_exists($this->sub, $GLOBALS['_'.$this->method]) && ( !$with ||  $GLOBALS['_'.$this->method][$this->sub] === $with) ;
- 			foreach ($this->navs as $navkey){
-	 			$this->is_nav = false;
+  			foreach ($this->navs as $navkey){
+ 	 			$this->is_nav = false;
   				if (array_key_exists($navkey, $GLOBALS['_'.$this->method])){
 	  				$this->is_nav = $navkey;
-	  				break;
+ 	  				break;
   				}
  			}
- 			$this->methodVars =  $GLOBALS['_'.$this->method];  //careful here!!
+ 			$pg = false;
+ 			if ($this->is_nav !== false ) {
+ 				$pgs = $this->derive_on_pg();
+	 			$this->on_pg=  $pgs[0];
+	 			$pg = isset($pgs[1])? $pgs[1] : $pgs[0];
+	 		}
+ 			$this->set_methodVars($pg);
  			return ($this->is_sub || $this->is_nav);
    		}
 	
+   		private  function set_methodVars($pg=false){ //careful here!!
+	   		if (isset($_SESSION['rm_form_'.$this->form_name])){ 
+		   		$this->methodVars = $_SESSION['rm_form_'.$this->form_name]; 
+		   	}
+ 			if (is_array($this->pages) && is_string($pg)){ 
+	 			$this->methodVars[$pg] = $GLOBALS['_'.$this->method];
+	 		}
+ 			else{ 
+	 			$this->methodVars = $GLOBALS['_'.$this->method];
+ 			}
+ 			$_SESSION['rm_form_'.$this->form_name] = $this->methodVars;
+   		}
+   		
+   		private  function set_methodVar($field,$val=''){ //careful here!!
+ 			if (is_array($this->pages)){ 
+	 			$this->methodVars[$this->pages[$this->on_pg]][$field] = $val;
+	 		}
+ 			else{ 
+	 			$this->methodVars[$field] =$val;
+ 			}
+ 			$_SESSION['rm_form_'.$this->form_name] = $this->methodVars;
+   		}
+	
+   		
 		function generate(){
 			$form_html  = "<form".$this->form_attrs().">\n";
 			$form_html .= $this->form_body();
@@ -128,55 +174,67 @@
 			return $form_html;
 		}
 		
+		function retrieve_var($field){
+			$val = null; 
+			if (!is_array($this->pages) && isset($this->methodVars[$field])){ $val = $this->methodVars[$field] ;}
+			if (is_array($this->pages) && isset($this->methodVars[$this->pages[$this->on_pg]][$field])){ $val = $this->methodVars[$this->pages[$this->on_pg]][$field] ;}
+			return $val;
+		}
+		
 		function get_value($field, $b='value="',$a='"'){
-			return  isset($this->methodVars[$field]) ? $b.$this->methodVars[$field].$a : '';
+			$val = $this->retrieve_var($field);
+			return  $val !== null ? $b.$val.$a : '';
 		}
 		
 		function is_checked($field){
-			return isset($this->methodVars[$field]) ?  'checked' : ''; 
+			return $this->retrieve_var($field) !== null ?  'checked' : ''; 
 		}
 		
-		function is_selected($field, $current, $sel='selected'){
-			if (!isset($this->methodVars[$field])) { return '';}
-			return $this->methodVars[$field] === $current ?  $sel : '' ;
+		function is_selected($field, $current, $target = false, $sel='selected'){
+			if ($target === false) { $target =  $this->retrieve_var($field); }
+			if ($target === null) { return '';}
+			return $target === $current ?  $sel : '' ;
 		}
 		
 		function post_process(){}
 		
 		function derive_on_pg(){
-			if (!is_string($this->is_nav)){ return $this->on_pg;}
+			if (!is_string($this->is_nav)){ return [$this->on_pg, $this->on_pg];}
 			$active = (substr($this->is_nav, 0, 3 ) == "g__") ? substr($this->is_nav, 3) : $this->is_nav;
-			return array_key_exists($active, $GLOBALS['_'.$this->method]) ? $GLOBALS['_'.$this->method][$active] : $this->on_pg;
+			$full =  array_key_exists($active, $GLOBALS['_'.$this->method]) ? $GLOBALS['_'.$this->method][$active] : $this->on_pg;
+			return explode('/', $full);
 		}
 		
 		function run($supress=false){
 			if ($this->checksub()){
-				if (!$this->is_processed && $this->run_validate()){
+				if (!$this->is_processed && $this->run_validate() && $this->is_sub){
 					$this->is_processed = $this->process();
 					if ($this->do_post) {
 						$this->post_process();
 						if ($this->do_post === "only") { return;}
 					}
 				}
-			};
-			if ($this->is_nav !== false) {$this->on_pg = $this->derive_on_pg();}
+			}; 			
 			if (!$supress){ echo $this->generate();}
 		}
 	
 	}
 	
 class myForm extends RM_form{
-	public 	$pages 		= ['pageit','formit'];
+	public 	$pages 		= ['formit', 'pageit'];
 	public  $sub_html	= '<input type="submit" name="submit" id="submit" value="Log In Here"/>';
 		
 	function validate(){
- 		if (!$this->methodVars['Pass']){ $this->setErr('Pass', 'A password is required'); }
-		if (!$this->methodVars['Uname']){ $this->setErr('Uname', 'A user name is required'); }
- 	  	if ($this->methodVars['Pass'] != $this->methodVars['Uname']){ { $this->setErr('Opps', 'hacker!!!'); }}
- 	  	for ($i=1; $i<5; $i++){
-	 	  if (!$this->methodVars['input'.$i]){ { $this->setErr('input'.$i, 'you are missing data at input'.$i); }}
- 		}
- 	  	if ($this->methodVars['input2'] != '2' ){ { $this->setErr('input2', 'must equal 2', '; '); }}
+		$current_pg = $this->pages[$this->on_pg];
+ 		if (!$this->methodVars[$current_pg]['Pass']){ $this->setErr('Pass', 'A password is required'); }
+		if (!$this->methodVars[$current_pg]['Uname']){ $this->setErr('Uname', 'A user name is required'); }
+ 	  	if ($this->methodVars[$current_pg]['Pass'] != $this->methodVars[$current_pg]['Uname']){ { $this->setErr('Opps', 'hacker!!!'); }}
+ 	  	if ($current_pg == 'formit'){
+	 	  	for ($i=1; $i<5; $i++){
+		 	  if (!$this->methodVars[$current_pg]['input'.$i]){ { $this->setErr('input'.$i, 'you are missing data at input'.$i); }}
+	 		}
+	 	  	if ($this->methodVars[$current_pg]['input2'] != '2' ){ { $this->setErr('input2', 'must equal 2', '; '); }}
+	 	}
 	}
 	protected function report($field, $tag="span", $attr="class=\"error\""){ return  isset($this->errs[$field]) ? "<$tag $attr>".$this->errs[$field]."</$tag>" :"";}
 
@@ -233,9 +291,6 @@ TEXT;
         </div>
         {$this->report('Opps', 'div')}
         <div>
-        $b
-        </div>
-        <div>
 	        <label>State: </label>
 	        <select name ="selectme"> {$state_o()}</select>
 	    </div>
@@ -252,13 +307,13 @@ TEXT;
 	function navigation_b($sub=false){
 		$nav = '';
 		if ($this->on_pg > 0){ 
-			$nav.= '<button type="submit" name="navform" id="back" value="'.($this->on_pg - 1).'"><< Previous</button>';
+			$nav.= '<button type="submit" name="navform" id="back" value="'.($this->on_pg -1).'/'.$this->on_pg.'"><< Previous</button>';
  		}
 		if ($this->on_pg+1 == $this->pg_ct){ 
 			$nav.=($sub ? $sub :  $this->sub_html);
 		}else if ($this->on_pg < $this->pg_ct){
 			$i = $this->on_pg + 1;
-			$nav.='<button type="submit" name="navform" id="next" value="'.($this->on_pg + 1).'">Next >></button>';
+			$nav.='<button type="submit" name="navform" id="next" value="'.($this->on_pg +1).'/'.$this->on_pg.'">Next >></button>';
  		}
 		return $nav ? "<div>$nav</div>": "";
 	}
@@ -273,18 +328,10 @@ TEXT;
 	}
 }
 	
-$_GET['submit']='Log In Here';
-$_GET['Pass']='Log In Here';
-$_GET['Uname']='Log In Here';
-$_GET['input1']='Log In Here';
-$_GET['input3']='Log In Here';
-// $_GET['input2']='2';
-$_GET['input4']='Log In Here';
-$_GET['check']='';
-$_GET['selectme']='val3';
-$form = new myForm( 'submit',array('mtd'=>'get'));
+$form = new myForm( 'submit',array('mtd'=>'get', 'navs'=>array('navform')));
 $form->run();
-
+//todo:
+//restrict navigation and validity to page/
 ?>
 
 
